8bb58be421ec0cdafff9cd36033ee8e4b4e31397
john-vasile <john.Vs11@hotmail.com>
2023-11-06 13:23:53 -0500

    basic setup

---
+++ mockdata.js
@@ -0,0 +1,29 @@
+export const resStocks = {
+    "count": 4,
+    "result": [
+      {
+        "description": "APPLE INC",
+        "displaySymbol": "AAPL",
+        "symbol": "AAPL",
+        "type": "Common Stock"
+      },
+      {
+        "description": "APPLE INC",
+        "displaySymbol": "AAPL.SW",
+        "symbol": "AAPL.SW",
+        "type": "Common Stock"
+      },
+      {
+        "description": "APPLE INC",
+        "displaySymbol": "APC.BE",
+        "symbol": "APC.BE",
+        "type": "Common Stock"
+      },
+      {
+        "description": "APPLE INC",
+        "displaySymbol": "APC.DE",
+        "symbol": "APC.DE",
+        "type": "Common Stock"
+      }
+    ]
+  }
