c9362df0bb2ef8fe5d8c7e540e40bf46cc53c62a
john-vasile <john.Vs11@hotmail.com>
2023-11-12 11:28:30 -0500

    adds Overview component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "./Card";
+import { mockCompanyDetails, resStocks } from "../mockdata";
 import Header from "./Header";
-import { resStocks } from "../mockdata";
+import Details from "./Details";
+import Overview from "./Overview";
+import Chart from "./Chart";
+import ThemeContext from "../context/ThemeContext";
 
 function Dashboard(props) {
+  const { darkMode } = useContext(ThemeContext);
+
   return (
     <div className="h-screen grid grid-cols-1 grid-rows-8 md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3 auto-rows-fr gap-4 p-10 font-quicksand">
-      <div className="col-span-1 md:col-span-2 xl:col-span-3 ">
+      <div className="col-span-1 md:col-span-2 xl:col-span-3 flex items-center">
         <Header name={resStocks.name} />
       </div>
       <div className="md:col-span-2 row-span-4 ">
         <Card>Chart</Card>
       </div>
-      <div className=" ">
-        <Card>Overview</Card>
+      <div className="row-span-1">
+        <Overview
+          symbol={mockCompanyDetails.ticker}
+          price={300}
+          change={30}
+          changePercent={10.0}
+          currency={"USD"}
+        />
       </div>
       <div className="row-span-2 xl:row-span-3 ">
         <Card>Details</Card>
---
+++ src/components/Overview.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "./Card";
+
+function Overview({ symbol, price, change }) {
+  return (
+    <Card>
+      Overview
+      <span className="absolute left-2 top-2 text-neutral-400 text-lg xl:text-xl 2xl:text-xl">
+        {symbol}
+      </span>
+      <div className="w-full h-full flex items-center justify-around pl-16">
+        <span className="text-2xl xl:text-3xl flex items-center">
+          ${price}
+          <span className="text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2">
+            USD
+          </span>
+        </span>
+
+        <span
+          className={`text-lg xl:text-xl 2xl:text-2xl ${
+            change > 0 ? "text-lime-500" : "text-red-500"
+          }`}
+        >
+          ${change} <span>({change}%)</span>
+        </span>
+      </div>
+    </Card>
+  );
+}
+
+export default Overview;
