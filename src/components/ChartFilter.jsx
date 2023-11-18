eed914273bc461093e99f556ab11c12f85ffef22
john-vasile <john.Vs11@hotmail.com>
2023-11-18 15:46:49 -0500

    adds hover effect to chart

--- src/components/ChartFilter.jsx
+++ src/components/ChartFilter.jsx
@@ -6,9 +6,11 @@ function ChartFilter({ text, active, onClick }) {
       onClick={onClick}
       className={`w-10 m-2 h-7 border rounded-md flex items-center justify-center cursor-pointer ${
         active
-          ? "bg-indigo-600   text-gray-100"
+          ? "bg-indigo-600 border-indigo-700  text-gray-100"
           : "border-indigo-500 text-indigo-300"
-      }`}
+      }
+      transition duration-300 hover:bg-indigo-600 hover:text-gray-100 hover:border-indigo-700
+      `}
     >
       {text}
     </button>
