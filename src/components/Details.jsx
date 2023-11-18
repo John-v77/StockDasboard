395b829b82ee0f1aface8612f78c5a0deb6fabe7
john-vasile <john.Vs11@hotmail.com>
2023-11-18 15:40:28 -0500

    adds darkTheme to Details component

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "./Card";
+import ThemeContext from "../context/ThemeContext";
 
 function Details({ details }) {
+  const { darkMode } = useContext(ThemeContext);
+
   const convertMilltoBillions = (number) => {
     return (number / 1000).toFixed(2);
   };
 
   return (
     <Card>
-      <ul className="w-full h-full flex flex-col justify-between divide-y-1">
+      <ul
+        className={`w-full h-full flex flex-col justify-between divide-y-1 ${
+          darkMode ? "divide-gray-600" : null
+        }`}
+      >
         {Object.keys(details).map((item) => {
           if (
             [
@@ -23,7 +30,10 @@ function Details({ details }) {
             return;
           }
           return (
-            <li key={item} className="flex-1 flex justify-between items-center">
+            <li
+              key={item}
+              className="flex-1 flex justify-between items-center py-1"
+            >
               <span>{item}</span>
               <span>
                 {item === "marketCapitalization"
